c4d15ebd9a337513a40c442562fac61c5f5151dd
yats1304 <chaubaly@gmail.com>
2026-01-11 19:33:55 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import AppSidebar from "@/components/layout/AppSidebar";
+import Navbar from "@/components/layout/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +27,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex`}
       >
-        {children}
+        <AppSidebar />
+        <main className="w-full">
+          <Navbar />
+          <div className="px-4">{children}</div>
+        </main>
       </body>
     </html>
   );
---
+++ src/components/layout/AppSidebar.tsx
@@ -0,0 +1,7 @@
+const AppSidebar = () => {
+  return (
+    <div className=''>AppSidebar</div>
+  )
+}
+
+export default AppSidebar
